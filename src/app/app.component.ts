7efa4b3f39aa887fb94fbe2862f95d7bee866168
Drozdovskiii <drozdovskiii@mail.ru>
2022-07-28 11:00:25 +0300

    Download project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { ProductsService } from './services/products.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-crash-course-2';
+export class AppComponent implements OnInit {
+  title = 'angular app';
+
+  products: IProduct[] = []
+  loading = false
+  term = ''
+  constructor(private productsService: ProductsService) { }
+
+  ngOnInit(): void {
+    this.loading = true
+    this.productsService.getAll().subscribe(products => {
+      this.products = products
+      this.loading = false
+    })
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ProductComponent} from './product/product.component';
+import { GlobalErrorComponent } from './global-error/global-error.component';
+import { FormsModule } from '@angular/forms';
+import { FilterProductsPipe } from './pipes/filter-products.pipe';
+import { FocusDirective } from './directives/focus.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    GlobalErrorComponent,
+    FilterProductsPipe,
+    FocusDirective,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
